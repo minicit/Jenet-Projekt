91a64564bd02b845516440e1fe3e223302652729
Nuelle94 <83752341+Nuelle94@users.noreply.github.com>
2021-05-06 10:10:10 +0200

    Update story.cs

--- Jenet Projekt/Entity/story.cs
+++ Jenet Projekt/Entity/story.cs
@@ -4,5 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace Jenet_Projekt.Entity
+{
+     Virologe:
+     "Es ist 8:00 Uhr morgens an einem sonnigen Montag. Du bist Karl Leiserbach, berühmter Virologe und erster Ansprechpartner bei Pandemien."
+     "Du sitzt am gedeckten Frühstückstisch und genießt deinen ersten Kaffee des Tages."
+     "Plötzlich klingelt das Telefon und als du den Hörer abnimmst, hörst du die Stimme des"
+     "Bundeskanzlers Angelo Merte, welcher dir voller Panik in der Stimme"
+     "von einem neuen Virus namens Corona berichtet, das sich rasend schnell auf der Welt ausbreitet."
+     "Du greifst dir sofort dein Desinfektionsmittel und deinen Mundschutz und machst dich auf den Weg, um das Virus zu bekämpfen."
+     "Als du aus der Tür trittst, siehst du bereits in der Ferne das erste Virus, auf dem Weg in dein beschauliches Dorf.
+     "KANNST DU DIE WELT RETTEN?"
+    
+     
+     Normalbürger:
+      "Es ist 8:00 Uhr morgens an einem sonnigen Montag. Du bist Max Mustermann, ein ganz normaler, langweiliger Bürger"
+      "mit ganz normalem, langweiligen Job und einem ganz normalen, langweiligen Leben."
+      "Du beißt gerade in dein Marmeladenbrötchen, als du die Stimme des Bundeskanzlers Angelo Merte in den Nachrichten hörst."
+      "Er berichtet von einem neuen Virus namens Corona, welcher die ganze Welt befallen hat."
+      "In deinem Kopf bildet sich eine verrückte Idee. Du beschließt aus deinem ganz normalem, langweiligen Leben auszubrechen"
+      "und die Welt zu retten. Im Küchenschrank findest du noch Desinfektionsmittel und ziehst damit los."
+      "Als du aus der Tür trittst, siehst du bereits in der Ferne das erste Virus, auf dem Weg in dein beschauliches Dorf.
+      "KANNST DU DIE WELT RETTEN?"
+      
+    
+      Coronaleugner:
+      "Es ist 8:00 morgens an einem sonnigen Montag. Du bist Attila Hildfrau, deutschlandweit bekannt für deine Verschwörungstheorien"
+      "und stolzer Besitzer einer großen Telegram-Gruppe. Du bist zufrieden, denn du hast gerade deine neueste Theorie mit deinen"
+      "Anhängern geteilt, um ihnen klar zu machen, dass tauben nur kameras sind, um uns zu überprüfen."
+      "Plötzlich erhältst du eine Nachricht von einem deiner Anhänger, dass die Regierung plant, uns mit einem Virus als Ausrede zuhause einzusperren."
+      "Das lässt du dir natürlich nicht gefallen! Du beschließt also, nach Berlin zu fahren, um die Regeirung zu stürzen."
+      "Als du aus der Tür trittst, siehst du bereits in der Ferne den ersten Regierungs-Mitarbeiter als Virus verkleidet, auf dem Weg ind ein ebschauliches Dorf."
+      "KANNST DU DIE REGIERUNG STÜRZEN?"
 
-namespace
+}
